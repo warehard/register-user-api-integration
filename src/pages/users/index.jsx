6ab32e89568e857e1aaaf91ef32bbaee7374649d
eduardomagno <eduardo.magno.u@gmail.com>
2020-08-19 12:54:05 -0300

    feat: users page: table and pagination frame

--- src/pages/users/index.jsx
+++ src/pages/users/index.jsx
@@ -1,10 +1,38 @@
 import React from 'react'
+import { Pagination, Table, Tag, Space } from 'antd'
+import Column from 'antd/lib/table/Column';
+
+
 
 const Users = () => {
+
+  function onShowSizeChange(current, pageSize) {
+    console.log(current, pageSize);
+  }
+
   return(
-    <div>
-      users
-    </div>
+    <>
+    <Table>
+      <Column title='id'></Column>
+      <Column title='Name'></Column>
+      <Column title='User'></Column>
+      <Column title='Email'></Column>
+      <Column title='Feedbacks'></Column>
+
+    </Table>
+
+    <br/>
+
+    <Pagination
+      showSizeChanger
+      onShowSizeChange={onShowSizeChange}
+      defaultCurrent={3}
+      total={500}
+    />
+    
+
+    
+    </>
   )
 }
 
